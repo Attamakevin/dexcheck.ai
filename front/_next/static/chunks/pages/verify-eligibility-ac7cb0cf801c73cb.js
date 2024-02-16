3af0ab86be7e67f00d236907548f2b3fbea6c2be
attamakevin <kevinejikez@gmail.com>
2024-02-17 00:52:50 +0100

    added front

---
+++ front/_next/static/chunks/pages/verify-eligibility-ac7cb0cf801c73cb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[374],{38262:function(n,_,i){(window.__NEXT_P=window.__NEXT_P||[]).push(["/verify-eligibility",function(){return i(71104)}])}},function(n){n.O(0,[774,888,179],(function(){return _=38262,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
